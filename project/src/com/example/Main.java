5ad1caf1d491d13fca626659d8a3a5585918d433
TejasGupta-27 <b22cs093@iitj.ac.in>
2025-03-05 15:17:34 +0530

    Resolved Syntax Error in index.html

--- project/src/com/example/Main.java
+++ project/src/com/example/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Debug: Application started hello");
         System.out.println("Debug: Application started hello");
+        System.out.println("Debug: Application started hello");
+        System.out.println("Debug: Application started hello");
+        System.out.println("Debug: Application started hello");
+        System.out.println("Debug: Application started hello");
+        System.out.println("Debug: Application started hello");
         System.out.println("Hello, World!");
     }
 }
